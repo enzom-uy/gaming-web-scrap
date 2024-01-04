e89f7ab6dd5e64a6e6b49443902e9f8f169fbe91
enzom-uy <enzom.dev@gmail.com>
2024-01-04 18:55:01 -0300

    separate scrapping functionality into another file and store into array

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
-use reqwest::{self};
-use scraper::{Html, Selector};
+mod scrap_url;
+
+use reqwest::{self, Error};
+use scrap_url::scrape_url;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let polygon_url = "https://www.polygon.com/gaming";
-    let response = reqwest::get(polygon_url).await;
 
-    match response {
-        Ok(response) => {
-            if !response.status().is_success() {
-                println!("Request error. Code: {}", response.status())
-            }
-
-            let body = response.text().await?;
-            let parsed_body = Html::parse_document(&body);
-
-            let selector = Selector::parse(r#"h2[class="c-entry-box--compact__title"]"#).unwrap();
-
-            for element in parsed_body.select(&selector) {
-                println!("Contenido: {}", element.text().collect::<String>());
-            }
+    match scrape_url(polygon_url, r#"h2[class="c-entry-box--compact__title"]"#).await {
+        Ok(titles) => {
+            println!("Contenido: {:?}", &titles);
         }
-
-        Err(e) => {}
+        Err(e) => {
+            println!("Error: {:?}", e)
+        }
     }
 
     Ok(())

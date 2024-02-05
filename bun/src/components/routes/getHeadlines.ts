57cc966486d3d7f7cc7223d7daa55a132b9a7f4d
enzom-uy <enzom.dev@gmail.com>
2024-02-05 19:01:28 -0300

    Bun build

--- bun/src/components/routes/getHeadlines.ts
+++ bun/src/components/routes/getHeadlines.ts
@@ -1,36 +1,10 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { Router, type Request, type Response } from 'express'
 import { prisma } from '../../config/prisma'
+import * as getHeadlinesControllers from '../controllers/getHeadlines.controller'
 
 const getHeadlines = Router()
 
-getHeadlines.get('/api/get-headlines', (req: Request, res: Response) => {
-    try {
-        const queryPage = Number(req.query.page)
-        const queryElementsPerPage = Number(req.query.elementsPerPage)
-        if (isNaN(queryPage) || isNaN(queryElementsPerPage)) {
-            return res
-                .send({
-                    error: 'Either queryPage or queryElementsPerPage are not number or are missing.',
-                })
-                .status(400)
-        }
-        const previousElements =
-            queryPage * queryElementsPerPage - queryElementsPerPage
-
-        prisma.headline
-            .findMany({
-                skip: queryPage <= 1 ? undefined : previousElements,
-                take: queryElementsPerPage,
-                cacheStrategy: { ttl: 60 },
-            })
-            .withAccelerateInfo()
-            .then((response) => {
-                res.send(response)
-            })
-    } catch (error) {
-        console.log(error)
-    }
-})
+getHeadlines.get('/api/get-headlines', getHeadlinesControllers.getHeadlines)
 
 export default getHeadlines
